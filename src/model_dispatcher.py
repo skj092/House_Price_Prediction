5e2c066a4486a755529981f12df72024410b1051
skj092 <sonujha092@gmail.com>
2021-02-02 07:50:42 +0000

    code formated using black

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,7 +1,7 @@
 from sklearn import linear_model
-from sklearn import ensemble 
+from sklearn import ensemble
 
 models = {
-    'linear_regression': linear_model.LinearRegression(),
-    'random_forest': ensemble.RandomForestRegressor(n_jobs=-1)
+    "linear_regression": linear_model.LinearRegression(),
+    "random_forest": ensemble.RandomForestRegressor(n_jobs=-1),
 }
